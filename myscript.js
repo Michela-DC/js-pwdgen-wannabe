ae4ccdb25e249b578bce1f4226e17fc34abb102a
Michela-DC <micheladelconte@outlook.com>
2022-02-03 18:48:36 +0100

    Replaced const with let

--- myscript.js
+++ myscript.js
@@ -1,17 +1,17 @@
-const userName = prompt('Ciao! Come ti chiami?')
+let userName = prompt('Ciao! Come ti chiami?')
 // quello che l'utente scriverà viene salvato dentro userName
 console.log(userName) 
 // stampo nella console cosa c'é dentro a userName per verificarne il contenuto
 
-const userSurname = prompt('Qual é il tuo cognome?')
+let userSurname = prompt('Qual é il tuo cognome?')
 console.log(userSurname)
 
-const userColor = prompt('Qual é il tuo colore preferito?')
+let userColor = prompt('Qual é il tuo colore preferito?')
 console.log(userColor)
 
 
-const HTMLsaluto = document.getElementById('saluto')  
-// Inizializzo la variabile const HTMLsaluto assegnadole l'elemento HTML che ha come id 'saluto' 
+let HTMLsaluto = document.getElementById('saluto')  
+// Inizializzo la variabile let HTMLsaluto assegnadole l'elemento HTML che ha come id 'saluto' 
 // The document object represents your web page. If you want to access any element in an HTML page, you always start with accessing the document object.
 
 HTMLsaluto.innerHTML += ' ' + userName + ' ' + userSurname
@@ -19,10 +19,10 @@ HTMLsaluto.innerHTML += ' ' + userName + ' ' + userSurname
 // += scorciatoia per concatenare, altrimenti per eseteso sarebbe HTMLsaluto.innerHTML = HTMLsaluto.innerHTML + ' ' + userName OPPURE usando il backtip HTMLsaluto.innerHTML += ` ${userName}
 // The innerHTML property sets or returns the HTML content (inner HTML) of an element
 
-const HTMLcolore = document.getElementById('colore')
+let HTMLcolore = document.getElementById('colore')
 
 HTMLcolore.innerHTML += ` ${userColor}`
 
-const HTMLpassword = document.getElementById('password')
+let HTMLpassword = document.getElementById('password')
 
 HTMLpassword.innerHTML += ` ${userName+userSurname+userColor+27}`
